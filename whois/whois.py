3de71c03ff6d82bd235fbd272df12d9900a9d9fa
amindrweb <amindrweb@gmail.com>
2025-05-21 18:42:33 +0200

    fix not found

--- whois/whois.py
+++ whois/whois.py
@@ -239,6 +239,8 @@ class NICClient:
 
             nhost = None
             response_str = response.decode("utf-8", "replace")
+            if response_str.strip().startswith("Domain not found."):
+                return "Domain Not Found"
             if 'with "=xxx"' in response_str:
                 return self.whois(query, hostname, flags, True, quiet=quiet, ignore_socket_errors=ignore_socket_errors)
             if flags & NICClient.WHOIS_RECURSE and nhost is None:
